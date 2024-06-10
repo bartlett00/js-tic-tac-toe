75985eceeb206836a0f91361bd12271c87b6337c
Pax <113464089+bartlett00@users.noreply.github.com>
2024-06-10 11:51:14 -0500

    Implement method for extracting rows, columns,     and diagonal value sets to check for a win.

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 function Gameboard() {
     const board = [];
-    const rows = 3;
-    const columns = 3;
 
     function Tile() {
         let value = 0;
-
         const XorO = (player) => {
             value = player;
         }
-
         const getValue = () => value;
-
         return { XorO, getValue };
     }
 
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < 3; i++) {
         board[i] = [];
-        for (let j = 0; j < columns; j++) {
+        for (let j = 0; j < 3; j++) {
             board[i].push(Tile());
         }
     }
@@ -40,13 +35,12 @@ function Gameboard() {
     return { playXorO, getBoard, printBoard };
 }
 
-
-
+//method for checking for ending the game(win or draw)
+//method for changing player turn (round based)
 function Controller(
     playerOne = 'player one', 
     playerTwo = 'player two') {
     const board = Gameboard();
-
     const players = [
         {
             name: playerOne,
@@ -72,7 +66,8 @@ function Controller(
     };
 
     const playRound = (row, column) => {
-        if(((board.getBoard())[row][column].getValue() === 0)) {
+        const selectedTile = (board.getBoard())[row][column];
+        if(selectedTile.getValue() === 0) {
             board.playXorO(row, column, getCurrentPlayer().symbol);
             changeTurn();
             printRound();
@@ -81,19 +76,73 @@ function Controller(
 
     const checkWin = () => {
         //check state of board
-        
+
+        const winConditions = {
+            row1: [1,1,1, 0,0,0, 0,0,0],
+            row2: [0,0,0, 1,1,1, 0,0,0],
+            row3: [0,0,0, 0,0,0, 1,1,1],
+            col1: [1,0,0, 1,0,0, 1,0,0],
+            col2: [0,1,0, 0,1,0, 0,1,0],
+            col3: [0,0,1, 0,0,1, 0,0,1],
+            diag1: [1,0,0, 0,1,0, 0,0,1],
+            diag2: [0,0,1, 0,1,0, 1,0,0]
+        };
+
+        let valueArr = [];
+        (board.getBoard()).map(row => {
+            row.map(tile => valueArr.push(tile.getValue()));
+        });
+        console.log(valueArr);
+/*
+        let playerTiles = {
+            row1: valueArr.slice(0, 3),
+            row2: valueArr.slice(3, 6),
+            row3: valueArr.slice(6),
+            col1: [valueArr[0], valueArr[3], valueArr[6]],
+            col2: [valueArr[1], valueArr[4], valueArr[7]],
+            col3: [valueArr[2], valueArr[5], valueArr[8]],
+            diag1: [valueArr[0], valueArr[4], valueArr[8]],
+            daig2: [valueArr[2], valueArr[4], valueArr[6]]
+        };
+*/
+        let playerTiles = [
+            valueArr.slice(0, 3),
+            valueArr.slice(3, 6),
+            valueArr.slice(6),
+            [valueArr[0], valueArr[3], valueArr[6]],
+            [valueArr[1], valueArr[4], valueArr[7]],
+            [valueArr[2], valueArr[5], valueArr[8]],
+            [valueArr[0], valueArr[4], valueArr[8]],
+            [valueArr[2], valueArr[4], valueArr[6]]
+        ];
+
+
+        const threeInARow = (tileSet) => tileSet.every(value => value === tileSet[0]);
+        console.log(threeInARow(playerTiles[0]));
+
+        playerTiles.forEach(array => {
+            if(threeInARow(array) == true) {
+                return true;
+            } else {
+                return false;
+            }
+        });
         //look for winner (8 ways of winning)
         //end game if winner/draw
     }
 
     printRound();
 
-    return {playRound, getCurrentPlayer};
+    return {playRound, getCurrentPlayer, checkWin};
 }
 
 const game = Controller();
 
 game.playRound(1, 2);
-game.playRound(1, 2);
+game.playRound(2, 1);
+game.playRound(1, 1);
+game.playRound(0,1);
+game.playRound(1, 0);
 
+console.log(game.checkWin());
 
